70b2299114419e2521155f99dbc66366b336aafc
Thyago Oliveira <taothyago@gmail.com>
2019-08-12 14:45:41 -0300

    :gem: Add routes and api

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    BrowserRouter,
+    Switch,
+    Route    
+} from 'react-router-dom'
+
+import Home from './pages/Home'
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' component={Home} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes
