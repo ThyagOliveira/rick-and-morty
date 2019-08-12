91a4062c8845e24b68dc1d007393bd25e76928f0
Thyago Oliveira <taothyago@gmail.com>
2019-08-12 14:46:10 -0300

    :gem: Add home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import api from '../../utils/api'
+import Card from '../../components/Card'
+import './styles.css'
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            characters: []
+        }
+    }
+    componentDidMount() {
+        this.loadCharacters()
+        api.loadCharacterPerPage(1).then((response) => {
+            console.log(response)
+        })
+    }
+    
+    loadCharacters = async () => {
+        const response = await api.loadCharacters()
+        this.setState({ characters: response.data.results })        
+    }
+
+    loadCharacterPerPage = async (page) => {
+        const response = await api.loadCharacterPerPage(page)
+        console.log(response)
+    }
+    render() {
+        return(
+            <div className="container">
+                {
+                    this.state.characters.map(element => (
+                        <Card
+                            key={element.id}
+                            source={element.image}
+                            name={element.name}
+                            id={element.id}
+                            status={element.status}
+                            species={element.species}
+                            gender={element.gender}
+                            origin={element.origin.name}
+                            location={element.location.name}
+                        />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Home
