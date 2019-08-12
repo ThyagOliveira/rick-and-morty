2957b00330f32eb8688f297852ee96eeb943c68f
Thyago Oliveira <taothyago@gmail.com>
2019-08-12 16:36:47 -0300

    :ambulance: :fire: Remover bootstrap imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import registerServiceWorker from './registerServiceWorker'
 
 
